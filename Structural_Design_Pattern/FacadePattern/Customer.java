591493d994054ea9032aa081fdf55884c4f9c166
Arpit Mathur <arpitmathur23@gmail.com>
2023-02-22 11:43:25 +0530

    adding comments

--- Structural_Design_Pattern/FacadePattern/Customer.java
+++ Structural_Design_Pattern/FacadePattern/Customer.java
@@ -4,13 +4,15 @@ import java.sql.SQLOutput;
 
 public class Customer {
     public static void main(String[] args) {
+//        without implementing facade
+        System.out.println("-----WITHOUT FACADE---");
         Ingredient ingredients=new Ingredient();
         Pizza pizza=new Pizza();
         String pizzaitems=ingredients.getPizzaItems();
         pizza.prepareFood(pizzaitems);
         System.out.println(pizza.deliverFood());
 
-
+//      implementing facade
         System.out.println("----------FACADE---------");
         System.out.println(Waiter.getFood(FoodType.PIZZA));
     }
